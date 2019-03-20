513cc8f795dfb6b5517a77c9b42b28afaec2b3b6
Md. Nafi shahriyar <32407364+paradoxnafi@users.noreply.github.com>
2019-03-20 09:24:59 +0600

    Supports python3.5.x
    At previous version of this code fstring feature of python3.6.x was used. As currently RaspbianOS using 3.5.x version of python, we are changing our code to support python3.5.x

--- text_to_voice.py
+++ text_to_voice.py
@@ -1,8 +1,10 @@
 import os
 import voice_to_text
 
+#Return value of speechToText() method from voice_to_text module is stored in text varibale
 text = voice_to_text.speechToText()
 
 def textToVoice():
-	os.system(f'echo "{text}" | festival --tts')
+	#System will speak the string in text
+	os.system('echo "%s" | festival --tts' %(text)) 
 	
