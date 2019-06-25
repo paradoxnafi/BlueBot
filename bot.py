03c7215260f809458fb59a344481e6e271831d85
paradoxnafi <paradoxnafi@protonmail.com>
2019-06-25 15:01:56 +0530

    Testing new features. They are currently commented

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import voice_to_text
 import text_to_voice as ttv
 
 text = voice_to_text.speechToText()
+
+
 if text.find('hello', 0) > -1:
 	ttv.textToVoice('Hello, I am happy to meet you')
 elif text.find('hi', 0) > -1:
@@ -12,9 +14,31 @@ elif text.find('what', 0) > -1:
 	if text.find('name', 0) > -1:
 		ttv.textToVoice('Currently I am called Blue Bot')
 elif text.find('tell', 0) > -1:
-	if text.find('joke', 0) > -1:
+	if teouxt.find('joke', 0) > -1:
 		ttv.textToVoice('Why don\'t you tell us a joke?')
-elif text.find('like', 0) > -1:
-	if text.find('talk', 0) > -1:
-		if text.find('people', 0) > -1:
-			ttv.textToVoice('Yes, I love to talk with people and learn new things.')
+
+'''
+
+Answers = [
+	'Hello, I am happy to meet you',
+	'Currently I am called Blue Bot',
+	"I don't eat anything.",
+	'I love green.',
+]
+
+Keywords = [
+	['hi', 'hello', 'how are you',],
+	['what is your name', 'what should I call you', 'who'],
+	['what', 'eat', 'like to eat', 'love to eat'],
+	['What', 'favourite', 'colour', 'love'],
+]
+
+Counter = []
+
+for i in range(len(Keywords)):
+	for j in range(len(Keywords[i])):
+		if Keywords[i][j] in text:
+			Counter[i] += 1
+Max = max(Counter)
+ttv.textToVoice(Answers[max])
+'''
