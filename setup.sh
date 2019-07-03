cf95c7773f587b93edb8347354cdf055c0e26f32
paradoxnafi <paradoxnafi@protonmail.com>
2019-07-03 16:42:55 +0530

    More data added

--- setup.sh
+++ setup.sh
@@ -11,3 +11,8 @@ pip3 install talkey speech_recognition
 apt install festival pidgin-festival festival-freebsoft-utils -y
 wget http://ftp.us.debian.org/debian/pool/main/f/festvox-us-slt-hts/festvox-us-slt-hts_0.2010.10.25-2_all.deb
 dpkg -i festvox-us-slt-hts_0.2010.10.25-2_all.deb
+
+# Natural language processing toolkit
+sudo pip3 install nltk numpy tflearn
+# I had issue with tensorflow. I had to compile and install manually
+# Need scipy and six pip module for https://github.com/paradoxnafi/tensorflow_chatbot
