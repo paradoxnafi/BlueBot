9b872504d9710b68cf49a3635ad92f5a80d18d30
paradoxnafi <paradoxnafi@protonmail.com>
2019-07-03 16:41:24 +0530

    Process answer using tensorflow

--- answer_processing.py
+++ answer_processing.py
@@ -1,19 +1,63 @@
 # Takes the Question as input(text) and returns answer as text
+
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import json
+import numpy
+import pickle
+import random
+import tflearn
+import tensorflow
 import voice_to_text
 
+with open("intents.json") as file:
+	data = json.load(file)
+
+with open("data.pickle", "rb") as f:
+	words, labels, training, output = pickle.load(f)
+
+tensorflow.reset_default_graph()
+# Nural network
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+model.load("model.tflearn")
+
+def bagOfWords(s, words):
+	bag = [0 for _ in range(len(words))]
+
+	s_words = nltk.word_tokenize(s)
+	s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+	for se in s_words:
+		for i, w in enumerate(words):
+			if w == se:
+				bag[i] = 1
+
+	return numpy.array(bag)
+
+
 def genAnswer():
-	Question = voice_to_text.speechToText()
-	Answers = ''
+	print("=> Bot is listening: ")
+	while True:
+		Question = voice_to_text.speechToText()
+		
+		results = model.predict([bagOfWords(Question, words)])[0]
+		results_index = numpy.argmax(results)
+		tag = labels[results_index]
 
-	if Question.find('hello', 0) > -1:
-		Answers = 'Hello, I am happy to meet you'
-	elif Question.find('hi', 0) > -1:
-		Answers = 'Hi, I am doing just fine'
-	elif Question.find('what', 0) > -1:
-		if Question.find('name', 0) > -1:
-			Answers = 'Currently I am called Blue Bot'
-	elif Question.find('tell', 0) > -1:
-		if teouxt.find('joke', 0) > -1:
-			Answers = 'Why don\'t you tell us a joke?'
-
-	return Answers
+		if results[results_index] > 0.7:
+			for tg in data["intents"]:
+				if tg["tag"] == tag:
+					responses = tg['responses']
+			Answers = random.choice(responses)
+			return Answers
+		else:
+			Answers = "Sorry. I am not sure what you mean."
+			return Answers
