0642323e8f4fcaf8a316b15c9a9f57dcafd44d3f
paradoxnafi <paradoxnafi@protonmail.com>
2019-07-03 16:44:01 +0530

    Will print the answer in terminal

--- text_to_voice.py
+++ text_to_voice.py
@@ -6,6 +6,7 @@ import answer_processing
 def textToVoice():
 	Answer = answer_processing.genAnswer()
 	#System will speak the string in Answer
+	print("Bot: " + Answer)
 	os.system('echo "%s" | festival --tts' %(Answer))
 	# Recursively calling this function to keep the program running
 	textToVoice()
